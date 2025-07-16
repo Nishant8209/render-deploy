16dc36e4f3b0f0b68a5337530e2f8c96bf44b070
Harihara <h37904139@gmail.com>
2025-07-16 14:11:30 +0530

    added new frontend path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,11 @@ const cors = require('cors');
 const morgan = require('morgan');
 const app: any = express();
 const cookieParser = require('cookie-parser');
-import serverless from 'serverless-http';
 
 import routes from './routes/index'
 import { errorResponse } from './utils/response'
 import path from 'path';
+const fs = require('fs');
 // Middleware
 app.use(express.json());
 app.use(cors({
@@ -26,8 +26,15 @@ app.use('/api', routes);
 const buildPath = path.join(__dirname, 'frontend');
 app.use(express.static(buildPath));
 
+
 app.get('/{*any}', (req:any, res:any) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    if (fs.existsSync(buildPath)) {
+  console.log('✅ Frontend build path exists:', buildPath);
+} else {
+  console.log('❌ Frontend build path does NOT exist:', buildPath);
+}
+  console.log('Serving index.html');
+  res.sendFile(path.join(__dirname, buildPath, "index.html"));
 });
 // Error handling middleware
 app.use((err: any, req: any, res: any, next: any) => {
@@ -35,4 +42,4 @@ app.use((err: any, req: any, res: any, next: any) => {
     errorResponse(res, 'Something went wrong!', 500, err)
 });
 
-module.exports = serverless(app);
+module.exports = app;
